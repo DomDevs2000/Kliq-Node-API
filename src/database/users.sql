4fcb2bdc0a86259e0b3f0df38703a5b5ce2fb507
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-15 21:34:24 +0000

    formatted statement

--- src/database/users.sql
+++ src/database/users.sql
@@ -6,13 +6,13 @@ GRANT ALL PRIVILEGES ON * . * TO 'joinkliq'@'localhost';
 CREATE DATABASE IF NOT EXISTS joinkliq;
 /* create new user */
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    application_id int(100) NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    application_id INT(100) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL, 
+    email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-phone_number VARCHAR(20) NOT NULL,
-photo_url VARCHAR(100) NOT NULL,
-referral_url VARCHAR(100) NOT NULL,
-);
+    phone_number VARCHAR(20) NOT NULL,
+    photo_url VARCHAR(100) NOT NULL,
+    referral_url VARCHAR(100) NOT NULL
+);;
