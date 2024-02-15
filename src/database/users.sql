5e27c56f44c3049d62ec2654bf3df3e7608e1488
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-15 21:19:23 +0000

    initial  setup of users.sql

---
+++ src/database/users.sql
@@ -0,0 +1,18 @@
+/* create new user */
+CREATE USER IF NOT EXISTS 'joinkliq'@'localhost' IDENTIFIED BY 'joinkliq';
+/*  give user  permissions*/
+GRANT ALL PRIVILEGES ON * . * TO 'joinkliq'@'localhost';
+/* create new database */
+CREATE DATABASE IF NOT EXISTS joinkliq;
+/* create new user */
+CREATE TABLE IF NOT EXISTS customers (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    application_id int(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL, 
+    password VARCHAR(100) NOT NULL,
+phone_number VARCHAR(20) NOT NULL,
+photo_url VARCHAR(100) NOT NULL,
+referral_url VARCHAR(100) NOT NULL,
+);
