39e756be2721ab732243d4f7ac22d243adfa25bc
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-15 17:17:16 +0000

    added import statements and comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-export const app = express();
-app.use(cors());
+const app = express();
+
+// express options
+app.use(cors()); // allow cors middleware
+
+// parse request body as json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// format json response
 app.set("json space", 2);
+
+export { app };
