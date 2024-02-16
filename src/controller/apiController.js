5006391cf9ae34c12091cac1163ea5b0bf8ed5c5
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-16 01:37:42 +0000

    prefixed unused req with underscore

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import { getAllUsers } from "../service/user.service.js";
 
-router.get("/users", async (req, res) => {
+router.get("/users", async (_req, res) => {
   try {
     const users = await getAllUsers();
     res.status(200).json(users); //TODO: console log retursn results but using userService doesnt return results here
