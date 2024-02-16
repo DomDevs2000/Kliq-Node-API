7be20f9382883fc97377060e29b2bc7d91d214f4
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-16 01:31:18 +0000

    deleted db.js - functionality is moved to user repo

--- src/database/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql";
-
-export const connection = mysql.createConnection({
-  host: "localhost",
-  user: "joinkliq",
-  password: "joinkliq",
-  database: "joinkliq_users",
-});
