9e22984ce01af43006a09de717f8c6df815a5fc7
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-16 01:01:48 +0000

    added user service function to controller

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,14 +1,11 @@
 import express from "express";
 const router = express.Router();
 import { connection } from "../database/db.js";
-
+import { getAllUsers } from "../service/user.service.js";
 router.get("/", async (req, res) => {
   try {
-    let sql = "SELECT * FROM users";
-    connection.query(sql, function (error, results, fields) {
-      if (error) throw error;
-      res.status(200).json(results);
-    });
+    const users = await getAllUsers();
+    res.status(200).json(users); //TODO: console log retursn results but using userService doesnt return results here
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: error });
