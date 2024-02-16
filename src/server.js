7c8f5eff575e737ac3db0cb85aa9143e0bb84a81
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-16 18:58:29 +0000

    refactored code to match user repository and service

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import { UserRepository } from "./database/user.repository.js";
+// import { UserRepository } from "./database/user.repository.js";
 import { app } from "./app.js";
 
-export const userRepository = new UserRepository();
+// export const userRepository = new UserRepository();
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
