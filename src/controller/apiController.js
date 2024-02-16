beb196b5288f77fb01e280d9dda9537d8b43af4c
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-16 00:10:47 +0000

    initial setup of express router

---
+++ src/controller/apiController.js
@@ -0,0 +1,18 @@
+import express from "express";
+const router = express.Router();
+import { connection } from "../database/db.js";
+
+router.get("/", async (req, res) => {
+  try {
+    let sql = "SELECT * FROM users";
+    connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      res.send(200).json(results);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: error });
+  }
+});
+
+export { router };
