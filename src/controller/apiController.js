91e79b900c0fabb83f17082cfe57cafeb07dd584
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-17 00:52:46 +0000

    removed TODO - fixed issue

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/users", async (_req, res) => {
   try {
     const users = await userService.getAllUsers();
-    res.status(200).json(users); //TODO: console log retursn results but using userService doesnt return results here
+    res.status(200).json(users);
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: error });
