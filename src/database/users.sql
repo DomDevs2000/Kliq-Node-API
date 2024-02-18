5697219f7a35efae700657b7c2fc3ee283c96190
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-18 19:14:56 +0000

    updated sql

--- src/database/users.sql
+++ src/database/users.sql
@@ -6,13 +6,14 @@ GRANT ALL PRIVILEGES ON * . * TO 'joinkliq'@'localhost';
 CREATE DATABASE IF NOT EXISTS joinkliq;
 /* create table */
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
     application_id INT(100)  NULL,
     first_name VARCHAR(50)  NULL,
     last_name VARCHAR(50)  NULL,
     email VARCHAR(100) UNIQUE  NULL,
     password VARCHAR(100)  NULL,
     phone_number VARCHAR(20)  NULL,
-    photo_url VARCHAR(100)  NULL,
-    referral_url VARCHAR(100) NULL
+    photo_url VARCHAR(255)  NULL,
+    referral_url VARCHAR(255) NULL,
+    PRIMARY KEY (id)
 );
