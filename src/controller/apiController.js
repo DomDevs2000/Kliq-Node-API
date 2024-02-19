7a7611396deb6f20a5693f847c1f7f74e8ff5ebe
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-19 18:28:20 +0000

    implemented id param, first and last name query validation

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -5,30 +5,48 @@ import { validationResult } from "express-validator";
 import {
   validateCreateUser,
   validateUpdateUser,
+  validateId,
+  validateFirstName,
+  validateLastName,
 } from "../middleware/userValidator.js";
+
 /*
 GET
 */
-router.get("/users", async (req, res) => {
-  try {
-    if (Object.keys(req.query).length) {
-      const users = await userService.findUserByName(req.query);
-      res.status(200).json(users);
-    } else {
-      const users = await userService.getAllUsers();
-      res.status(200).json(users);
+router.get(
+  "/users",
+  validateId,
+  validateFirstName,
+  validateLastName,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      } else if (Object.keys(req.query).length) {
+        const users = await userService.findUserByName(req.query);
+        res.status(200).json(users);
+      } else {
+        const users = await userService.getAllUsers();
+        res.status(200).json(users);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: error });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ error: error });
-  }
-});
+  },
+);
 
-router.get("/users/:id", async (req, res) => {
+router.get("/users/:id", validateId, async (req, res) => {
   try {
-    const userId = req.params.id;
-    const userById = await userService.findUserById(userId);
-    res.status(200).json(userById);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      const userId = req.params.id;
+      const userById = await userService.findUserById(userId);
+      res.status(200).json(userById);
+    }
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: error });
@@ -56,7 +74,7 @@ router.post("/users", validateCreateUser, async (req, res) => {
 /*
  DELETE
  */
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/:id", validateId, async (req, res) => {
   try {
     const deletedUserId = req.params.id;
     if (!deletedUserId) {
@@ -75,7 +93,7 @@ router.delete("/users/:id", async (req, res) => {
 /*
  UPDATE
 */
-router.put("/users/:id", validateUpdateUser, async (req, res) => {
+router.put("/users/:id", validateId, validateUpdateUser, async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
--- src/middleware/userValidator.js
+++ src/middleware/userValidator.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, query, param } from "express-validator";
 
 export const validateCreateUser = [
   body("application_id").notEmpty().withMessage("First name is required"),
@@ -20,4 +20,19 @@ export const validateUpdateUser = [
   body("phone_number").notEmpty().withMessage("Phone number is required"),
   body("photo_url").isURL().withMessage("Invalid photo URL"),
   body("referral_url").isURL().withMessage("Invalid referral URL"),
+];
+
+export const validateId = [
+  param("id").optional().isInt().withMessage("Invalid ID"),
+];
+
+export const validateFirstName = [
+  query("first_name")
+    .optional()
+    .notEmpty()
+    .withMessage("First name is required"),
+];
+
+export const validateLastName = [
+  query("last_name").optional().notEmpty().withMessage("Last name is required"),
 ];
