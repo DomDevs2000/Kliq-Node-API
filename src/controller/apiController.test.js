6923282fbb57295ba1e874f82e9af4b91bd34024
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-19 17:31:50 +0000

    updated tests - included tests for POST PUT and DELETE

--- src/controller/apiController.test.js
+++ src/controller/apiController.test.js
@@ -1,5 +1,5 @@
-import { describe, it, expect } from "vitest";
 import request from "supertest";
+import { describe, expect, it } from "vitest";
 import { app } from "../app.js";
 
 describe("API Controller tests", () => {
@@ -8,22 +8,52 @@ describe("API Controller tests", () => {
     expect(res.status).toBe(200);
   });
   it("should return a user by id", async () => {
-    const res = await request(app).get("/api/users/id/1");
+    const res = await request(app).get("/api/users/1");
 
     expect(res.body).toEqual([
       {
-        application_id: null,
+        id: "1",
+        application_id: 0,
         first_name: "Tech",
         last_name: "Support",
         email: "info@remotecoach.fit",
         password:
           "$2y$10$n3293NxnekVK1Sry/OO.f.gc9WvvbwZ2IcOELjIkjfeKns2GREpAO",
-        phone_number: null,
+        phone_number: "NULL",
         photo_url:
           "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
-        referral_url: null,
+        referral_url: "NULL",
       },
     ]);
+    expect(res.status).toBe(200);
+  });
+  it("POST /users should create a new user", async () => {
+    const newUser = {
+      application_id: "2000",
+      first_name: "Jane",
+      last_name: "Doe",
+      email: "Jo@Doe.org.uk",
+      password: "testpass",
+      phone_number: null,
+      photo_url:
+        "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+      referral_url: null,
+    };
+
+    const res = await request(app).post("/users").send(newUser);
+    expect(res.status).toBe(200);
+  });
+
+  it("PUT /users/:id should update a user by id", async () => {
+    const res = await request(app).put("/users/13").send({
+      first_name: "Jane",
+    });
+    expect(res.status).toBe(200);
+  });
+  it("DELETE /users/:id should delete a user by id", async () => {
+    const res = await request(app).delete(
+      "/users/0ecc5e07-e10b-4fd1-a005-b3c27aeea720",
+    );
     expect(res.status).toBe(200);
   });
 });
