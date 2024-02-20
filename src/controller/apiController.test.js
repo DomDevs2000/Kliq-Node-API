61ce580594091200383020827daa47389860ede5
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-20 10:26:20 +0000

    moved middleware tests to api controller test

--- src/controller/apiController.test.js
+++ src/controller/apiController.test.js
@@ -83,3 +83,165 @@ describe("API Controller tests", () => {
     expect(res.status).toBe(200);
   });
 });
+
+describe("create user validation request", () => {
+  it("should return 400 if user request with missing first name", async () => {
+    const user = {
+      application_id: 0,
+      // Missing first_name
+      last_name: "Doe",
+      email: "Jane@Doeeee.org.uk",
+      password: "$2b$10$RRCXCwBTwF36PrDgNhgSl.EW9a6atbQUvCtATz7iT1gY831EwIyCe",
+      phone_number: "07414114152",
+      photo_url:
+        "https://prod-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+      referral_url:
+        "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+    };
+    const res = await request(app).post("/api/users").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          msg: "First name is required",
+          path: "first_name",
+          location: "body",
+        },
+      ],
+    });
+  });
+  it("should return 400 error if create user request with missing last name", async () => {
+    const user = {
+      application_id: 0,
+      first_name: "Janny",
+      // missing last name
+      email: "Jane@Doeeeeeeeeeeeee.org.uk",
+      password: "$2b$10$RRCXCwBTwF36PrDgNhgSl.EW9a6atbQUvCtATz7iT1gY831EwIyCe",
+      phone_number: "07414114152",
+      photo_url:
+        "https://prod-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+      referral_url:
+        "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+    };
+    const res = await request(app).post("/api/users").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          msg: "Last name is required",
+          path: "last_name",
+          location: "body",
+        },
+      ],
+    });
+  });
+});
+
+describe("first name query string validation test", () => {
+  it("should 400 error if empty query string", async () => {
+    const res = await request(app).get("/api/users?first_name=");
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          value: "",
+          msg: "First name is required",
+          path: "first_name",
+          location: "query",
+        },
+      ],
+    });
+  });
+});
+describe("last name query string validation test", () => {
+  it("should 400 error if empty query string", async () => {
+    const res = await request(app).get("/api/users?last_name=");
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          value: "",
+          msg: "Last name is required",
+          path: "last_name",
+          location: "query",
+        },
+      ],
+    });
+  });
+});
+describe("Id param validator test", () => {
+  it("should 400 error if bad  id param request", async () => {
+    const res = await request(app).get("/api/users/e");
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          value: "e",
+          msg: "Invalid ID",
+          path: "id",
+          location: "params",
+        },
+      ],
+    });
+  });
+});
+
+describe("Update user validation request", () => {
+  it("should 400 error if bad update user request with missing first name", async () => {
+    const user = {
+      application_id: 0,
+      // Missing first_name
+      last_name: "Doe",
+      email: "Jane@Doeeee.org.uk",
+      password: "$2b$10$RRCXCwBTwF36PrDgNhgSl.EW9a6atbQUvCtATz7iT1gY831EwIyCe",
+      phone_number: "07414114152",
+      photo_url:
+        "https://prod-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+      referral_url:
+        "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+    };
+    const res = await request(app).put("/api/users/1").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          msg: "First name is required",
+          path: "first_name",
+          location: "body",
+        },
+      ],
+    });
+  });
+  it("should 400 error if bad update user request with missing last name", async () => {
+    const user = {
+      application_id: 0,
+      first_name: "Janny",
+      // missing last name
+      email: "Jane@Doeeeeeeeeeeeee.org.uk",
+      password: "$2b$10$RRCXCwBTwF36PrDgNhgSl.EW9a6atbQUvCtATz7iT1gY831EwIyCe",
+      phone_number: "07414114152",
+      photo_url:
+        "https://prod-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+      referral_url:
+        "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+    };
+    const res = await request(app).put("/api/users/1").send(user);
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      errors: [
+        {
+          type: "field",
+          msg: "Last name is required",
+          path: "last_name",
+          location: "body",
+        },
+      ],
+    });
+  });
+});
