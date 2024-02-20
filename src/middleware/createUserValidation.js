aac9d3ed354d254b62b5fc7523c8b89b00b18998
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-20 01:44:31 +0000

    updated message to match field

--- src/middleware/createUserValidation.js
+++ src/middleware/createUserValidation.js
@@ -3,7 +3,7 @@ import { body } from "express-validator";
 export const validateCreateUser = [
   body("application_id").notEmpty().withMessage("First name is required"),
   body("first_name").notEmpty().withMessage("First name is required"),
-  body("last_name").notEmpty().withMessage("First name is required"),
+  body("last_name").notEmpty().withMessage("Last name is required"),
   body("email").isEmail().withMessage("Invalid email address"),
   body("password").notEmpty().withMessage("Password is required"),
   body("phone_number").notEmpty().withMessage("Phone number is required"),
