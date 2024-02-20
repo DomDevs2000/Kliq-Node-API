fa846eb42cb9dcec00d889cf573545d064f15a05
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-20 01:24:46 +0000

    fixed comment formatting

--- scripts/users.sh
+++ scripts/users.sh
@@ -1,13 +1,13 @@
 !#/bin/bash
 # sets up database
-#PASSWORD IS YOUR MYSQL ROOT PASSWORD
+# PASSWORD IS YOUR MYSQL ROOT PASSWORD
 mysql -u root -p joinkliq_users <src/database/users.sql
-#imports csv file
-#PASSWORD IS joinkliq
+# imports csv file
+# PASSWORD IS joinkliq
 mysqlimport --ignore-lines=1 --fields-terminated-by=',' --verbose --local -u joinkliq -p joinkliq_users src/database/users.csv
 
-#install dependencies
+# install dependencies
 npm install
 
-#run the server
+# run the server
 npm run start
