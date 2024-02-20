f2eff0920d2cf075a056f725f36c95e63ccba2b6
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-02-20 10:14:01 +0000

    added mock to test

--- src/controller/apiController.test.js
+++ src/controller/apiController.test.js
@@ -1,6 +1,32 @@
 import request from "supertest";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { app } from "../app.js";
+vi.mock("../database/user.repository.js", async () => {
+  const user = {
+    id: 1,
+    application_id: null,
+    first_name: "Tech",
+    last_name: "Support",
+    email: "info@remotecoach.fit",
+    password: "$2y$10$n3293NxnekVK1Sry/OO.f.gc9WvvbwZ2IcOELjIkjfeKns2GREpAO",
+    phone_number: null,
+    photo_url:
+      "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+    referral_url: null,
+  };
+  return {
+    default: {
+      getAllUsers: vi.fn().mockResolvedValue([user]),
+      findUserById: vi.fn().mockResolvedValue([user]),
+      findUserByFirstName: vi.fn().mockResolvedValue([user]),
+      findUserByLastName: vi.fn().mockResolvedValue([user]),
+      findUserByBothNames: vi.fn().mockResolvedValue([user]),
+      createUser: vi.fn().mockResolvedValue([]),
+      updateUser: vi.fn().mockResolvedValue([]),
+      deleteUser: vi.fn().mockResolvedValue([]),
+    },
+  };
+});
 
 describe("API Controller tests", () => {
   it("GET should return 200", async () => {
@@ -12,18 +38,17 @@ describe("API Controller tests", () => {
 
     expect(res.body).toEqual([
       {
-        id: "1",
-        application_id: 0,
-        first_name: "Janny",
-        last_name: "Doe",
-        email: "Jane@Doeeeeeee.org.uk",
+        id: 1,
+        application_id: null,
+        first_name: "Tech",
+        last_name: "Support",
+        email: "info@remotecoach.fit",
         password:
-          "$2b$10$RRCXCwBTwF36PrDgNhgSl.EW9a6atbQUvCtATz7iT1gY831EwIyCe",
-        phone_number: "07414114152",
+          "$2y$10$n3293NxnekVK1Sry/OO.f.gc9WvvbwZ2IcOELjIkjfeKns2GREpAO",
+        phone_number: null,
         photo_url:
-          "https://prod-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
-        referral_url:
           "https://prod-img-cdn.remotecoach.fit/assets/86a13ad1-5404-43ed-b3cf-e35f9663c871/image_picker_E51BD2CE-EBE7-4E29-AF38-FCC808EF0E10-745-000000384888B683.png",
+        referral_url: null,
       },
     ]);
     expect(res.status).toBe(200);
